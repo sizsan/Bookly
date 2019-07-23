5dba479c34b6118afd66f36e7c7e08e53e48678f
sizsan <sizsan2001@gmail.com>
2019-07-23 15:29:40 +1000

    User model Generatored

--- db/migrate/20190723052620_devise_create_users.rb
+++ db/migrate/20190723052620_devise_create_users.rb
@@ -32,7 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
+      t.string :username,  null: false
+      t.string :firstname, null: false 
+      t.string :lastname,  null: false
 
       t.timestamps null: false
     end
