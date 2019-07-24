b5370c8b31da6660f66b8b630ece1baf605d9e7c
sizsan <sizsan2001@gmail.com>
2019-07-24 14:47:37 +1000

    Advert finalised

--- db/migrate/20190724041043_create_adverts.rb
+++ db/migrate/20190724041043_create_adverts.rb
@@ -1,8 +1,9 @@
 class CreateAdverts < ActiveRecord::Migration[5.2]
   def change
     create_table :adverts do |t|
-      t.references :book, foreign_key: true
-      t.decimal :price, precision: 10, scale: 2
+      t.references :book, foreign_key: true, null: false
+      t.references :seller, foreign_key: {to_table: :users}
+      t.decimal :price, precision: 10, scale: 2, null: false
       t.string :condition
 
       t.timestamps
