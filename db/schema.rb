daacc04ed85facecb709103fee021aeeaa007ca7
Tony Huynh <huynh.tony55@gmail.com>
2019-07-24 14:46:07 +1000

    created book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_052620) do
+ActiveRecord::Schema.define(version: 2019_07_24_044340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.integer "isbn"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,9 +28,9 @@ ActiveRecord::Schema.define(version: 2019_07_23_052620) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
-    t.string "firstname"
-    t.string "lastname"
+    t.string "username", null: false
+    t.string "firstname", null: false
+    t.string "lastname", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
