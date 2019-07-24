f17fa1622909289d6175443892ec532cc8ba9edd
Tony Huynh <huynh.tony55@gmail.com>
2019-07-24 14:58:00 +1000

    added null: false

--- db/migrate/20190724044340_create_books.rb
+++ db/migrate/20190724044340_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.integer :isbn
-      t.string :title
+      t.integer :isbn, null: false
+      t.string :title, null: false
 
       t.timestamps
     end
