e9c012e7753b718a89d3020be17061434f379532
sizsan <sizsan2001@gmail.com>
2019-07-29 11:33:15 +1000

    order method created

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,4 +1,17 @@
 class Advert < ApplicationRecord
   belongs_to :book
   belongs_to :seller, class_name: "User"
+
+  def self.order_by_user
+    adverts=[]
+    Advert.order(:users).each do |user|
+      adverts += user.adverts.order(:book_id)
+    end
+    return adverts
+  end
+
+  def self.order_by_price
+    return Advert.order(:price)
+  end 
+
 end
