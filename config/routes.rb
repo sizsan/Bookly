e26ede67eba50daab63f0598cb165862f5ca03e3
sizsan <sizsan2001@gmail.com>
2019-07-29 15:48:21 +1000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'adverts/index'
-  devise_for :users
-  root to: "exchange#index" 
-  resources :exchange
-  resources :adverts
-=======
   get 'books/index'
   devise_for :users
   root to: "exchange#index" 
   resources :exchange
   resources :books
->>>>>>> master
+  resources :adverts
 end
