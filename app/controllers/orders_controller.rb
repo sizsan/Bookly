b1921371437db93e87cfe98733a4c3339a35b0ac
sizsan <sizsan2001@gmail.com>
2019-07-31 10:25:07 +1000

    new page complete

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
+    def new
+        @order = Order.new
+        @advert = Advert.find(params[:advert_id])
+    end
+
+    def create
+        @order = Order.create_order(params[:advert][:book_id], params[:advert][:price], params[:advert][:condition], params[:advert][:seller_id])
+        redirect_to 
+    end
 end
