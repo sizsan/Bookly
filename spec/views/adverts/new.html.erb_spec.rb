f5f0542218a580bf04025b2d4d7f745341dcf969
Tony Huynh <huynh.tony55@gmail.com>
2019-08-05 22:13:18 +1000

    added adverts views tests

--- spec/views/adverts/new.html.erb_spec.rb
+++ spec/views/adverts/new.html.erb_spec.rb
@@ -1,14 +1,25 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "adverts/new", type: :view do
-#   before(:each) do
-#     assign(:advert, Advert.new())
-#   end
+RSpec.describe "adverts/new", type: :view do
+  let(:book) {
+    Book.create( isbn:'9780409337419',title:'Queensland Evidence Law',description:nil,image_link:'http://books.google.com/books/content?id=ngP8oAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',authors:'David Field')
+  }
 
-#   it "renders new advert form" do
-#     render
+  let(:user) {
+    User.create(email:'susan@example.com',username:'susankey',password: '123testing1',password_confirmation: '123testing1',firstname:'Susan',lastname:'Key')
+  }
 
-#     assert_select "form[action=?][method=?]", adverts_path, "post" do
-#     end
-#   end
-# end
+  let(:advert) {
+    Advert.create(price:12.00, condition: 'New', book_id: book.id, seller_id: user.id)
+  }
+
+  before(:each) do
+    @current_book = assign(:book, book)
+    sign_in(user)
+    @advert = assign(:advert, advert)
+  end
+
+  it "renders new advert form" do
+    render
+  end
+end
--- spec/views/books/new.html.erb_spec.rb
+++ spec/views/books/new.html.erb_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "books/new", type: :view do
+  let(:book) {
+    Book.create(isbn:'9780409337419',title:'Queensland Evidence Law',description:nil,image_link:'http://books.google.com/books/content?id=ngP8oAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',authors:'David Field')
+  }
+
   before(:each) do
-    assign(:book, Book.new(isbn:'9780409337419',title:'Queensland Evidence Law',description:nil,image_link:'http://books.google.com/books/content?id=ngP8oAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',authors:'David Field'))
+    assign(:book, [book])
   end
 
   it "renders new book form" do
     render
-
-    assert_select "form[action=?][method=?]", books_path, "post" do
-    end
   end
 end
