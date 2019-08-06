b69ad502f5a3069bb1fb244a312c7400c7b9c080
sizsan <sizsan2001@gmail.com>
2019-08-06 11:53:26 +1000

    cleaning

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -3,18 +3,6 @@ class Advert < ApplicationRecord
   belongs_to :seller, class_name: "User"
   has_one_attached :cover
 
-  def self.order_by_user
-    adverts=[]
-    Advert.order(:users).each do |user|
-      adverts += user.adverts.order(:book_id)
-    end
-    return adverts
-  end
-
-  def self.order_by_price
-    return Advert.order(:price)
-  end 
-
   def self.order_by_created
     return Advert.order(:created_at).reverse_order
   end
